e6d599c4396a4ab6bebe646569c673b9c53764d5
s10930013 <131859345+s10930013@users.noreply.github.com>
2023-05-16 11:37:11 +0800

    Update sketch_may16a.ino

--- sketch_may16a.ino
+++ sketch_may16a.ino
@@ -1,5 +1,5 @@
 #include <SimpleDHT.h>
-int pinDHT11 = 4;
+int pinDHT11 = 27;
 SimpleDHT11 dht11(pinDHT11);
 
 
@@ -9,13 +9,13 @@ void setup() {
   Serial.begin(115200);
   pinMode(15, OUTPUT);//選告GPIO15作為輸出（綠色LED）
   pinMode(2, OUTPUT);//選告GPIO 2作為輸出（黃色LED)
-  pinMode(0, OUTPUT);//選告GPIO 0作為輸出（黃色LED）
+  //pinMode(0, OUTPUT);//選告GPIO 0作為輸出（黃色LED）
   pinMode(4, OUTPUT);//選告GPIO 4作為輸出（紅色LED）
   //pinMode(13, INPUT);//選告GPIO 13作為輸入（溫溼度讀取）
 }
 
 void loop() {
-  
+  Serial.println("Sample DHT11...");
   byte temperature = 0;
   byte humidity = 0;
   int err = SimpleDHTErrSuccess;
@@ -25,11 +25,9 @@ void loop() {
   Serial.print("Sample OK: ");
   Serial.print((int)temperature); Serial.print(" *C, "); 
   Serial.print((int)humidity); Serial.println(" H");  
-    delay(100);
   // DHT11 sampling rate is 1HZ
+  delay(1500);
  
-  //LightValue = analogRead(13); //讀取的GPIO 13讀取的數值放在LightValue
- // Serial.println(LightValue); //LightValue顯示在序列視窗
   if ((int)humidity < 50) {
     //小於500，代表亮度充足，關閉所有LED
     digitalWrite(15, LOW);
@@ -39,13 +37,11 @@ void loop() {
   }
 
 
- .
-
   
   if ((int)humidity>= 50 && (int)humidity < 60) {
     //500-1000之間，代表亮度中等
     digitalWrite(15, HIGH);
-    digitalWrite(2, LOW);
+    digitalWrite(2,LOW);
     digitalWrite(4, LOW);
   }
   if ((int)humidity >= 60 && (int)humidity < 70) {
@@ -60,5 +56,5 @@ void loop() {
     digitalWrite(2, HIGH);
     digitalWrite(4, HIGH);
   }
-  delay(100);
+  delay(1500);
 }
